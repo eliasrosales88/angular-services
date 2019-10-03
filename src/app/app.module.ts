95de4ab4190692c4cfacc3c5ebdc2d48db7c73b6
Elias Rosales <eliasrosales88@gmgmail.com>
2019-10-04 01:53:05 +0200

    Adding components and list service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentAComponent } from './component-a/component-a.component';
+import { ComponentBComponent } from './component-b/component-b.component';
+import { ListService } from './service/list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentAComponent,
+    ComponentBComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
